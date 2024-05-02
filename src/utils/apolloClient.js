56b426d8ecd248d1e35085e818dc2434a79eb936
Andrei Faur <bogdan.andrei.faur@gmail.com>
2024-05-02 15:23:16 +0200

    Revert "Instalacion y configuracion de apolloClient"
    This reverts commit c498b4f51a7259eb4905bd5456bbf76543d23514.

--- src/utils/apolloClient.js
+++
@@ -1,9 +0,0 @@
-import { ApolloClient } from "apollo-boost";
-
-const createApolloClient = () => {
-    return new ApolloClient({
-        uri: "http://192.168.1.36:4000/graphql"
-    })
-}
-
-export default createApolloClient;
